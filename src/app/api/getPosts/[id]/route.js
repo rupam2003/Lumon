582e84172c2b4129e00da59abf095f6337a1b021
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-08-19 20:12:14 +0530

    Create route.js

---
+++ src/app/api/getPosts/[id]/route.js
@@ -0,0 +1,27 @@
+
+import UserPost from "@/app/models/userpost";
+import { connectDb } from "@/app/utils/connectDb"
+import { NextResponse } from "next/server"
+
+export async function GET(req , {params}) {
+    try {
+
+        await connectDb()
+      
+        console.log(params.id);
+        
+        const posts = await UserPost.find().populate("author")
+        console.log(posts);
+        
+
+        return NextResponse.json({posts})
+
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({
+            
+            message : "Error fetching post"})
+    }
+}
+

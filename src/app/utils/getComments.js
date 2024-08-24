f8f2579301b3259b59c9b29a563cc33411a93e1d
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-08-24 16:50:17 +0530

    Update getComments.js

--- src/app/utils/getComments.js
+++ src/app/utils/getComments.js
@@ -1,5 +1,5 @@
 export const getComments = async (postId) => {
-    const res = await fetch(`https://socials1212.vercel.app/api/getComments/${postId}`, { cache : "no-store"})
+    const res = await fetch(`https://lum-on.vercel.app/api/getComments/${postId}`, { cache : "no-store"})
     const {comments} = await res.json()
     return comments
 }

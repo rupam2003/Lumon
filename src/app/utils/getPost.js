34a9e0822f1951b886f23f3dc1c118e417af59f1
rupam2003 <139889957+rupam2003@users.noreply.github.com>
2024-08-24 16:49:12 +0530

    Update getPost.js

--- src/app/utils/getPost.js
+++ src/app/utils/getPost.js
@@ -1,6 +1,6 @@
 
 export const getPost = async (id) => {
-    const res = await fetch(`https://socials1212.vercel.app/api/getPost/${id}`, { cache : "no-store"})
+    const res = await fetch(`https://lum-on.vercel.app/api/getPost/${id}`, { cache : "no-store"})
     const post = await res.json()
     return post
 }
